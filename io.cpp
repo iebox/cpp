19782eeac521aa5b7619a9dfc5af761b0556cb46
mike.xie <mike.xie@me.com>
2021-02-20 21:19:17 +0800

    io test

---
+++ io.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    printf("Please input some numbers:\n");
+    cin >> a >> b >> c;
+
+    cout << "your input is:";
+    cout << a << b << c;
+
+    return 0;
+}
